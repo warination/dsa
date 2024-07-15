fc016f24fc862686103fccc32ae7fa2057d2de18
Warish Raaz <warination@gmail.com>
2024-07-15 13:57:00 +0530

    5th

---
+++ customArrayMethods.js
@@ -0,0 +1,64 @@
+class anArray {
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    return this.length;
+  }
+
+  get(index) {
+    return this.data[index];
+  }
+
+  pop() {
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return lastItem;
+  }
+
+  shift() {
+    const lastItem = this.data[0];
+    for (let i = 0; i < this.length; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+    delete this.data[this.length - 1];
+    this.length--;
+    return lastItem;
+  }
+
+  unshift(item) {
+    for (let i = this.length; i > 0; i--) {
+      this.data[i] = this.data[i - 1];
+    }
+    this.data[0] = item;
+    this.length++;
+    return this.length;
+  }
+
+  deleteByIndex(index) {
+    const item = this.data[index];
+    for (let i = index; i < this.length; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+    delete this.data[this.length - 1];
+    this.length--;
+    return item;
+  }
+}
+
+const myArr = new anArray();
+myArr.push("hello");
+myArr.push("world");
+myArr.push("welcome");
+myArr.push("to");
+myArr.push("my");
+myArr.push("javascript tutorial");
+
+console.log(myArr);
+console.log(myArr.unshift(12));
+console.log("---------", myArr);
--- flatnestedarray.js
+++ flatnestedarray.js
@@ -2,7 +2,7 @@ const nestedArray = [1, [2, [3, [4, [5]], 6], 7]];
 const flatArray = nestedArray.reduce((acc, curr) => {
   return acc.concat(Array.isArray(curr) ? flattenArray(curr) : curr);
 }, []);
-console.log(flatArray); // Output: [1, 2, 3, 4, 5, 6, 7]
+console.log(flatArray);
 
 function flattenArray(arr) {
   return arr.reduce((acc, curr) => {
